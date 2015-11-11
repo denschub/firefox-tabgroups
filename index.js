098bd9e9f2adeded29452ed5249113868e5aaee2
Dennis Schubert <mail@dennis-schubert.de>
2015-11-11 08:49:39 +0100

    Implement read API to the current SessionStorage

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const tabsUtils = require("sdk/tabs/utils");
-const windowUtils = require("sdk/window/utils");
+const WindowUtils = require("sdk/window/utils");
 
 const Ui = require("lib/ui");
-const { SessionStorage } = require("lib/storage/session");
+const {SessionStorage} = require("lib/storage/session");
 
 const Storage = new SessionStorage();
 
@@ -22,12 +21,10 @@ groupsPanel.on("hide", () => {
 });
 
 groupsPanel.on("show", () => {
-  let currentBrowser = tabsUtils.getTabBrowser(
-    windowUtils.getMostRecentBrowserWindow()
-  );
+  let currentWindow = WindowUtils.getMostRecentBrowserWindow();
 
   groupsPanel.port.emit(
     "TablistChange",
-    Storage.getTabs(currentBrowser)
+    Storage.getTabs(currentWindow)
   );
 });
