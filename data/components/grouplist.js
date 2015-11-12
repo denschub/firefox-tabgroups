3bf1e3ad483eb36703ad5c0a729c13ed81b94766
Dennis Schubert <mail@dennis-schubert.de>
2015-11-12 02:37:01 +0100

    Iterate over groups

--- data/components/grouplist.js
+++ data/components/grouplist.js
@@ -1,10 +1,26 @@
-const GroupList = React.createClass({
-  render: function() {
-    return (
-      React.createElement(
-        Group,
-        {}
-      )
-    );
-  }
-});
+const GroupList = (() => {
+  const GroupListStandalone = React.createClass({
+    render: function() {
+      return (
+        React.DOM.div(
+          {},
+          this.props.groups.map((group) => {
+            return React.createElement(
+              Group,
+              {
+                key: group.id,
+                group: group
+              }
+            )
+          })
+        )
+      );
+    }
+  });
+
+  return ReactRedux.connect((state) => {
+    return {
+      groups: state.get("tabgroups")
+    };
+  }, ActionCreators)(GroupListStandalone);
+})();
