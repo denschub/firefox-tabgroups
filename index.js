a1c602fb11514c6e9f822055fe85bf5afdc8cee0
Dennis Schubert <mail@dennis-schubert.de>
2015-11-13 03:25:18 +0100

    Add preference for enabling the shortcut

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const WindowUtils = require("sdk/window/utils");
 
 const Ui = require("lib/ui");
+const Prefs = require("sdk/simple-prefs");
 const {SessionStorage} = require("lib/storage/session");
 
 const Storage = new SessionStorage();
@@ -8,12 +9,20 @@ const Storage = new SessionStorage();
 const groupsPanel = Ui.GroupsPanel();
 const showPanelButton = Ui.PanelButton();
 
-Ui.GroupsHotkey(() => {
-  if (groupsPanel.isShowing) {
-    groupsPanel.hide();
-  } else {
-    groupsPanel.show({position: showPanelButton});
-    showPanelButton.state("window", {checked: true});
+let hotkey = null;
+
+if (Prefs.prefs.bindPanoramaShortcut) {
+  hotkey = Ui.GroupsHotkey(groupsPanel, showPanelButton);
+}
+
+Prefs.on("bindPanoramaShortcut", () => {
+  if (Prefs.prefs.bindPanoramaShortcut) {
+    if (!hotkey) {
+      hotkey = Ui.GroupsHotkey(groupsPanel, showPanelButton);
+    }
+  } else if (hotkey) {
+    hotkey.destroy();
+    hotkey = null;
   }
 });
 
