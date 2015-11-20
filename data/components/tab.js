f582fe58e9f80ee5f4a3711e3bba4bc15cd00c4f
Dennis Schubert <mail@dennis-schubert.de>
2015-11-20 09:24:38 +0100

    Add favicon

--- data/components/tab.js
+++ data/components/tab.js
@@ -5,16 +5,29 @@ const Tab = React.createClass({
   },
 
   render: function() {
+    let favicon;
+    if (this.props.tab.icon) {
+      favicon = React.DOM.img({
+        alt: "",
+        className: "tab-icon",
+        src: this.props.tab.icon
+      });
+    }
+
     let tabClasses = classNames({
       active: this.props.tab.active,
       tab: true
     });
 
     return (
-      React.DOM.li({
-        className: tabClasses,
-        onClick: this.handleTabClick
-      }, this.props.tab.title)
+      React.DOM.li(
+        {
+          className: tabClasses,
+          onClick: this.handleTabClick
+        },
+        favicon,
+        React.DOM.span({className: "tab-title"}, this.props.tab.title)
+      )
     );
   },
 
