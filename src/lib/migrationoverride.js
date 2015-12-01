8af968fce85b0b7a3fef2b68b0d8b66f30502b6f
Dennis Schubert <mail@dennis-schubert.de>
2015-12-01 02:56:01 +0100

    Fix my English

--- src/lib/migrationoverride.js
+++ src/lib/migrationoverride.js
@@ -1,8 +1,8 @@
 const { Cu } = require("chrome");
 
-const DummyMigrator = {
+const DummyMigration = {
   migrate: function() {
-    // I'm a happy little migrator
+    // I'm a happy little migration
     // and I'm doing nothing.
   }
 };
@@ -10,7 +10,7 @@ const DummyMigrator = {
 exports.MigrationOverride = function() {
   try {
     let migrator = Cu.import("resource:///modules/TabGroupsMigrator.jsm", {});
-    migrator.TabGroupsMigrator = DummyMigrator;
+    migrator.TabGroupsMigrator = DummyMigration;
   } catch (e) {
     return;
   }
