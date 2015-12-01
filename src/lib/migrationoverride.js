de98c764d4ddbbe47ee309e653399b42f42421e9
Dennis Schubert <mail@dennis-schubert.de>
2015-12-01 02:59:43 +0100

    Add comment for clarification

--- src/lib/migrationoverride.js
+++ src/lib/migrationoverride.js
@@ -1,5 +1,12 @@
 const { Cu } = require("chrome");
 
+/**
+ * This overrides the TabView migration as added in Bug 1221050 with a
+ * migration that basically does nothing. Since our addon is compatible with
+ * the original data, there is nothing we have to do here.
+ *
+ * This will get removed some time in the future.
+ */
 const DummyMigration = {
   migrate: function() {
     // I'm a happy little migration
